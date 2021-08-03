33558622fe9960834dd9f0d0046d433141869712
fwils001 <fwils001@ucr.edu>
2021-08-03 16:05:49 -0700

    added functions

--- js/app.js
+++ js/app.js
@@ -6,7 +6,28 @@ class Pet {
         this.boredom = boredom
         this.age = age
     }
+eat() {
+    alert("Yum yum!")
 }
+sleep() {
+    console.log(this.sleepiness);
+    alert("Yawn! I'm so tired!")
+}  
+    
+excite(){
+    alert("Hee hee!")
+}   
+
+getOlder(){
+    alert("It's my birthday!")
+}
+}
+
 const tomagotchi = new Pet("", 0, 0, 0, 0)
 console.log(tomagotchi)
+
+tomagotchi.eat();
+tomagotchi.sleep();
+tomagotchi.excite();
+tomagotchi.getOlder();
 
