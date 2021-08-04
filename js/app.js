4643ce715f3217650f26ec33f1dafd10efd1cc3b
fwils001 <fwils001@ucr.edu>
2021-08-04 16:30:26 -0700

    added if statements

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+function display(){
+    document.getElementById("demo").innerHTML=document.getElementById("tt").value;	
+    //I found this function on stack overflow: https://stackoverflow.com/questions/58750774/ask-user-enter-name-with-javascript-and-html
+}
+
 class Pet {
     constructor(name) {
         this.name = name;
@@ -5,52 +10,33 @@ class Pet {
         this.sleepiness = 1;
         this.boredom = 1;
         this.age = 0
-        this.dead = false;
-        this.time
+        this.dead = false
+        this.lifetime = 0
     }
 
-eat() {
-    alert("Yum yum!")
-    return this.hunger
+screenMovement() {
+    const move = ["flex-start", "flex-end","center"][Math.floor(Math.random()*3)];
+    return move;
 }
-
-eats() {
-    this.hunger--
-}
-
-eating(amount) {
-    this.hunger-=amount
-}
-
-hunger() {
+    
+getHungry() {
+    alert("Feed me!")
     this.hunger++
+    const hunger = document.querySelector('#hunger')
+    hunger.innerHTML = this.hunger
 }
 
-sleep() {
+getSleepy() {
     alert("Yawn! I'm so tired!")
-    return this.sleepiness
+    //return this.sleepiness
+    this.sleepiness++
 }  
 
-sleeps() {
-    this.sleepiness--
-}
-
-sleeping(amount) {
-    this.sleepiness-=amount
-}
-
-excite(){
+getBored() {
     alert("Hee hee!")
-    return this.boredom
+    //return this.boredom
+    this.boredom++
 }   
-
-excites() {
-    this.boredom--
-}
-
-exciting(amount) {
-    this.boredom-=amount
-}
 
 getOlder(){
     alert("It's my birthday!")
@@ -62,21 +48,34 @@ getsOlder(amount) {
         this.getOlder()
     }
 }
+endOfLife() {
+    alert("You killed your Tomagotchi! No more pets for you!")
+    //$('img').attr("src", )
 }
-function display(){
-    document.getElementById("demo").innerHTML=document.getElementById("tt").value;		
-}
-//I found this function on stack overflow: https://stackoverflow.com/questions/58750774/ask-user-enter-name-with-javascript-and-html
 
-const catchphrases = [
-    'Yum yum!',
+relocate() {
+    //$('#image,').css('justify-content', `${this.screenPositions()}`)
+    //$('#image,').css('align-items', `${this.screenPositions()}`)
+}
+
+transform(){
+    //$('img').css('width', '20%');
+    //$('img').css('height', '50%');
+    if(this.lifetime >= 150) {
+    //    $('img').attr('src', 
+    }    
+
+
+
+/*const catchphrases = [
+    'Feed me!',
     'Yawn! I am so tired!',
     'Hee hee!',
     'Today is my birthday!'
 ]
 console.log(catchphrases)
 
-/*let catchphraseList = document.createElement('div')
+let catchphraseList = document.createElement('div')
 catchphraseList.setAttribute('id', 'catchphrases')
 for(let i = 0; i < catchphrases.length; i++) {
   let articleTag = document.createElement('article')
@@ -87,13 +86,76 @@ for(let i = 0; i < catchphrases.length; i++) {
     console.log(catchphraseList)
 }
 */
-
+}
+}
 const tomagotchi = new Pet("", 0, 0, 0, 0)
 console.log(tomagotchi)
 
 
-//tomagotchi.eat();
+const game = {
+    myPet: null,
+    intervalID: null,
+    time:0,
+    lightsOn: false,
+    light: false,
+
+    start(name) {
+        const newPet2 = new Pet(name);
+        this.newPet = newPet2;
+       // $('#name').text(this.myPet.name)
+        //$('#input name').val('')
+
+        this.startTimer();
+
+    },
+    startTimer() {
+        this.intervalID = setInterval(() => {
+            
+            this.time++
+            this.newPet.lifetime++
+            this.reasonOfDeath = "";
+            const timeInMilliseconds = this.time * 1000
+
+            if (timeInMilliseconds % 2000 === 0) {
+                this.newPet.relocate();
+            }
+            if (timeInMilliseconds % 5000 === 0) {
+                this.newPet.getHungry();
+            }
+            if (timeInMilliseconds % 15000 === 0) {
+                this.newPet.getBored()
+            }
+            if (timeInMilliseconds %3000 === 0) {
+                this.newPet.getSleepy();
+            }
+            if (timeInMilliseconds % 6000 === 0) {
+                this.newPet.getOlder();
+                this.newPet.transform();
+                this.time = 0;
+            }
+            if (this.newPet.hunger>=10) {
+                this.myPet.dead = true;
+                this.reasonOfDeath = "starved and withered into tomagotchi bones";
+            }
+            if (this.myPet.sleepiness>=10){
+                this.newPet.dead = true;
+                this.reasonOfDeath = "pulled too many all-nighters so eyes fell out";     
+            }
+            if (this.myPet.boredom>=10) {
+                this.newPet.dead = true;
+                this.reasonOfDeath = "skull shattered from restlessness";
+            }
+            if (this.newPet.dead) {
+                clearInterval(this.intervalID)
+                this.newPet.graveyard(this.reasonOfDeath)
+            }                      
+        }, 100) 
+    },  
+
+//tomagotchi.getHungry();
+//tomagotchi.endOfLife();
 //tomagotchi.sleep();
 //tomagotchi.excite();
 //tomagotchi.getOlder();
-
+//tomagotchi.transform();
+        
