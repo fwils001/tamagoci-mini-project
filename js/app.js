09a6639dc6bc26afa6e28cfcdcee99405192397e
fwils001 <fwils001@ucr.edu>
2021-08-05 16:54:46 -0700

    added stuff

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-function display(){
-    document.getElementById("demo").innerHTML=document.getElementById("tt").value;	
+//function display(){
+   // document.getElementById("demo").innerHTML=document.getElementById("tt").value;	
     //I found this function on stack overflow: https://stackoverflow.com/questions/58750774/ask-user-enter-name-with-javascript-and-html
-}
+//}
 
 class Pet {
     constructor(name) {
@@ -22,35 +22,34 @@ screenMovement() {
 getHungry() {
     alert("Feed me!")
     this.hunger++
-    const hunger = document.querySelector('#hunger')
-    hunger.innerHTML = this.hunger
+    const hunger = document.getElementById('#hunger')
+    hunger.innerHTML = `hunger: ${this.hunger}`
 }
+    
 
 getSleepy() {
     alert("Yawn! I'm so tired!")
     //return this.sleepiness
     this.sleepiness++
-}  
+    this.sleepiness.innerHTML = `sleepiness: ${this.sleepiness}`
+}
 
 getBored() {
-    alert("Hee hee!")
+    alert("Sigh! What is life?")
     //return this.boredom
     this.boredom++
+    this.boredom.innerHTML = `boredom: ${this.boredom}`
 }   
 
 getOlder(){
     alert("It's my birthday!")
     this.age++
+    this.age.innerHTML = `age: ${this.age}`
 }
 
-getsOlder(amount) {
-    for (let i=0; i < amount; i++) {
-        this.getOlder()
-    }
-}
 endOfLife() {
     alert("You killed your Tomagotchi! No more pets for you!")
-    //$('img').attr("src", )
+    let src = document.getElementById('image').getAttribute('src','https://www.google.com/imgres?imgurl=http%3A%2F%2Fclipart-library.com%2Fimages%2FAibjGLK4T.gif&imgrefurl=http%3A%2F%2Fclipart-library.com%2Fcartoon-dead-people.html&tbnid=INyLPE33sYFWlM&vet=12ahUKEwiTpeXHxZryAhUVsZ4KHeTVDGkQMygDegUIARC8AQ..i&docid=VPt5ougOgXtFLM&w=490&h=538&itg=1&q=dead%20cartoon&hl=en&ved=2ahUKEwiTpeXHxZryAhUVsZ4KHeTVDGkQMygDegUIARC8AQ');
 }
 
 relocate() {
@@ -59,33 +58,10 @@ relocate() {
 }
 
 transform(){
-    //$('img').css('width', '20%');
-    //$('img').css('height', '50%');
     if(this.lifetime >= 150) {
-    //    $('img').attr('src', 
+        const img = document.querySelector("#image")
+        img.style.width="900px"
     }    
-
-
-
-/*const catchphrases = [
-    'Feed me!',
-    'Yawn! I am so tired!',
-    'Hee hee!',
-    'Today is my birthday!'
-]
-console.log(catchphrases)
-
-let catchphraseList = document.createElement('div')
-catchphraseList.setAttribute('id', 'catchphrases')
-for(let i = 0; i < catchphrases.length; i++) {
-  let articleTag = document.createElement('article')
-    //articleTag.setAttribute('id', catchphrases[i])
-    articleTag.innerHTML = `<h1>${catchphrases[i]}</h1>`
-    //catchphraseList.appendChild(articleTag)
-    //document.body.appendChild(catchphraseList)
-    console.log(catchphraseList)
-}
-*/
 }
 }
 const tomagotchi = new Pet("", 0, 0, 0, 0)
@@ -93,17 +69,21 @@ console.log(tomagotchi)
 
 
 const game = {
-    myPet: null,
+    newPet: null,
     intervalID: null,
     time:0,
     lightsOn: false,
     light: false,
 
     start(name) {
+        const inputname=document.getElementById('tt')
+        const nameDisplay=inputname.value
+        this.newPet=nameDisplay
         const newPet2 = new Pet(name);
         this.newPet = newPet2;
-       // $('#name').text(this.myPet.name)
-        //$('#input name').val('')
+        const textName=document.getElementById('name')
+        textName.textContent = this.nameDisplay
+
 
         this.startTimer();
 
@@ -115,6 +95,7 @@ const game = {
             this.newPet.lifetime++
             this.reasonOfDeath = "";
             const timeInMilliseconds = this.time * 1000
+
 
             if (timeInMilliseconds % 2000 === 0) {
                 this.newPet.relocate();
@@ -128,20 +109,20 @@ const game = {
             if (timeInMilliseconds %3000 === 0) {
                 this.newPet.getSleepy();
             }
-            if (timeInMilliseconds % 6000 === 0) {
+            if (timeInMilliseconds % 60000 === 0) {
                 this.newPet.getOlder();
                 this.newPet.transform();
                 this.time = 0;
             }
             if (this.newPet.hunger>=10) {
-                this.myPet.dead = true;
+                this.newPet.dead = true;
                 this.reasonOfDeath = "starved and withered into tomagotchi bones";
             }
-            if (this.myPet.sleepiness>=10){
+            if (this.newPet.sleepiness>=10){
                 this.newPet.dead = true;
                 this.reasonOfDeath = "pulled too many all-nighters so eyes fell out";     
             }
-            if (this.myPet.boredom>=10) {
+            if (this.newPet.boredom>=10) {
                 this.newPet.dead = true;
                 this.reasonOfDeath = "skull shattered from restlessness";
             }
@@ -149,13 +130,66 @@ const game = {
                 clearInterval(this.intervalID)
                 this.newPet.graveyard(this.reasonOfDeath)
             }                      
-        }, 100) 
+            }, 100) 
     },  
 
-//tomagotchi.getHungry();
-//tomagotchi.endOfLife();
-//tomagotchi.sleep();
-//tomagotchi.excite();
-//tomagotchi.getOlder();
-//tomagotchi.transform();
-        
+    feedTomagochi() {
+        if(this.newPet.hunger>1){
+            this.newPet.hunger--
+            const hunger = document.getElementById('#hunger')
+            hunger.innerHTML = `hunger: ${this.newPet.hunger}`
+        }
+    }, 
+    
+    playPet() {
+        if (this.newPet.boredom>1) {
+            this.newPet.boredom--
+            const boredom = document.getElementById('#boredom')
+            boredom.innerHTML = `boredom: ${this.newPet.boredom}`
+        }
+    },
+    lightsOut() {
+        if (this.light) {
+            if (this.newPet.sleepiness >1) {
+                this.newPet.sleepiness--
+                const sleepy = document.getElementById('#sleepiness')
+                sleepy.innerHTML = `sleepiness: ${this.newPet.sleepiness}`
+            }
+        } else {
+            this.light = true;
+        }
+    }
+}
+
+//EVENT LISTENERS
+let form = document.getElementById('nameform')
+    form.addEventListener('click', (event) =>{
+let submitName = document.getElementById('tt').value;
+document.querySelector('#tomagotchi-name').innerHTML = `${submitName}`
+    game.start(submitName)
+})
+
+let button1 = document.getElementById('feed')
+    button1.addEventListener('click', (event) =>{
+        event.preventDefault();
+let feedPet = document.getElementById('hunger').value;
+document.querySelector('#moreButtons').innerHTML = `${feedPet}`
+    })
+ 
+let button2 = document.getElementById('turn-off-lights')
+    button2.addEventListener('click', (event) =>{
+        event.preventDefault();
+let sleepPet = document.getElementById('sleepiness').value;
+document.querySelector('#moreButtons').innerHTML = `${sleepPet}`
+    })
+
+let button3 = document.getElementById('play')
+    button3.addEventListener('click', (event) =>{
+        event.preventDefault();
+let playPet = document.getElementById('boredom').value;
+document.querySelector('#moreButtons').innerHTML = `${playPet}`
+    })
+
+    
+ 
+    
